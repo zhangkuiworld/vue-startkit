2ca4c28858ff3783f6cca79e75368bb79ebdf956
zhangkui <zhangkui@100tal.com>
2016-10-21 18:03:34 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 let Vue = require('vue');
 let VueRouter = require("vue-router");
+let VueResource = require("vue-resource");
 const helloVue = require('./views/hello.vue');
 const worldVue = require('./views/world.vue');
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 const routes = [
     {path:"/hello",component:helloVue},
